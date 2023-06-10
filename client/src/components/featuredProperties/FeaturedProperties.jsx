947326d2188b2369cf665d3e2c0f64a3d87269c4
Lucas Passeti <lucaspasseti10@gmail.com>
2023-06-10 13:47:38 -0300

    pull

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,35 +1,36 @@
-import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
+import useFetch from '../../hooks/useFetch';
 
 const FeaturedProperties = () => {
-  const { data, loading } = useFetch("/services?featured=true&limit=4");
+    const { data, loading } = useFetch("/services?featured=true&limit=4");
 
-  return (
-    <div className="fp">
-      {loading ? (
-        "Loading"
-      ) : (
-        <>
-          {data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <img
-                src={item.photos[0]}
-                alt=""
-                className="fpImg"
-              />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">Preço inicial ${item.cheapestPrice}</span>
-              {item.rating && <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excelent</span>
-              </div>}
-            </div>
-          ))}
-        </>
-      )}
-    </div>
-  );
-};
 
-export default FeaturedProperties;
+    return (
+        <div className="fp">
+        {loading ? (
+          "Loading"
+        ) : (
+          <>
+            {data.map((item) => (
+              <div className="fpItem" key={item._id}>
+                <img
+                  src={item.photos[0]}
+                  alt=""
+                  className="fpImg"
+                />
+                <span className="fpName">{item.name}</span>
+                <span className="fpCity">{item.city}</span>
+                <span className="fpPrice">Preço Inicial R${item.cheapestPrice}</span>
+                {item.rating && <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>}
+              </div>
+            ))}
+          </>
+        )}
+      </div>
+    );
+  };
+
+export default FeaturedProperties
